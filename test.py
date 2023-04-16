bb6cc8b0a2f0be32b39ad18edd4ce9be9727e878
unknown <jiyitong44@gmail.com>
2023-04-16 11:07:56 +0800

    print number of test

--- test.py
+++ test.py
@@ -124,6 +124,7 @@ if __name__ == '__main__':
     logger.info('==========================')
     test_dataset = CandidatesDataset(test_ucands)
     test_loader = get_dataloader(test_dataset, batch_size=128, shuffle=False, num_workers=0)
+    print('number of test: ', len(test_u))
     preds = model.full_rank(test_u) # np.array (u, topk)
     #preds: [num_users, num_items]
     final_preds = [] #preds after filtering
